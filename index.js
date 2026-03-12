0759217acf04777728d6e08bcd5a966638db8f86
krishviash <krishvivash23@gmail.com>
2026-03-12 22:12:09 +1100

    emailjs working

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-// template_1d8hbpa
-// service_jdbct1c
-// Z-Gw2XWEl5DQim18D
-
 function contact(event) {
     event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible"
 
-    console.log('test') 
+    emailjs
+    .sendForm(
+        'service_jdbct1c',
+        'template_1d8hbpa',
+        event.target,
+        'Z-Gw2XWEl5DQim18D'
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible")
+        success.classList += " modal__overlay--visible"
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible")
+        alert(
+            "The email service is tremporarily unavailable. Please contact me directly on krishvivash23@gmail.com"
+        )
+    })
 }
+
